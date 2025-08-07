f0d103806db17ccf9e3ae9912b5e9f623f659bbf
aleasad11 <ali@devtechfusion.com>
2025-08-07 13:43:37 +0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
    <div>
-    <h1>App</h1>
+    <h1>App new</h1>
     <p>Count: {count}</p>
     <button onClick={() => setCount(count + 1)}>Increment</button>
    </div>
