405e925adab6abb54ab63a5b737e209299f079ce
haseebahmed3131 <Haseeb.ahmed3131@gmail.com>
2025-09-12 11:58:06 +0500

    Added cors for local development.

--- config/cors.php
+++ config/cors.php
@@ -22,6 +22,8 @@ return [
     'allowed_origins' => [
       env('FRONTEND_URL', 'http://crm.holisticare.pk'),
       env('FRONTEND_URL_SECURE', 'https://crm.holisticare.pk'),
+      'http://127.0.0.1:8000',
+      'http://localhost:8000',
     ],
 
     'allowed_origins_patterns' => [],
