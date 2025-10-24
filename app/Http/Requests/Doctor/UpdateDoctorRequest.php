965bdd27cf2254ac9cf368e859552cc591c788e9
haseebahmed3131 <Haseeb.ahmed3131@gmail.com>
2025-10-24 14:55:25 +0500

    Remove validation on update doctor.

--- app/Http/Requests/Doctor/UpdateDoctorRequest.php
+++ app/Http/Requests/Doctor/UpdateDoctorRequest.php
@@ -22,8 +22,8 @@ class UpdateDoctorRequest extends FormRequest
     public function rules(): array
     {
         return [
-            // 'name' => 'sometimes|required|string|max:255',
-            'name' => 'sometimes|required|string|max:255|regex:/^[a-zA-Z\s]+$/',
+            'name' => 'sometimes|required|string|max:255',
+            // 'name' => 'sometimes|required|string|max:255|regex:/^[a-zA-Z\s]+$/',
             'phone_number' => 'sometimes|required|string|max:20',
             'department_id' => 'sometimes|required|exists:departments,id',
             // 'procedures' => 'sometimes|required|array|min:1',
